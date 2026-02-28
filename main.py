9ed4153335956cd46e7b1eb0e9b920807a6f8c4c
Asim khan <asimkhanr1@gmail.com>
2026-02-28 06:02:07 +0500

    update

--- main.py
+++ main.py
@@ -4,8 +4,26 @@ from mcp.server.fastmcp import FastMCP
 # Initialize FastMCP server
 mcp = FastMCP(name="My FastMCP Server", stateless_http=True) # when true we dont need handshake or inilitaze thing. Enable stateless HTTP mode!  when stateless_http is True,  FastMCP will not maintain any session state for HTTP requests, making it more scalable and suitable for serverless environments.
 
+
+# Important: Get the FastAPI/Starlette app instance
 mcp_app = mcp.streamable_http_app() # Enable streamable HTTP app mode! when streamable_http_app is True, FastMCP will allow streaming responses for HTTP requests, enabling real-time data delivery and improved performance for long-running tasks.
 
+
+@mcp_app.get("/")
+# async def root():
+#     return {"message": "Welcome to My FastMCP Server!"}
+async def home():
+    return{
+        "message": "Welcome to My FastMCP Server!",
+        "tools": [
+            {"name": "greet", "description": "Greet the user with a message"},
+            {"name": "add", "description": "Add two numbers"},
+            {"name": "weather", "description": "Get weather information for a city"}
+        ],
+        "status": "online and ready to accept requests",
+        "message": "MCP Server is running and ready to accept requests!",
+        "endpoints": ["/docs", "/openapi.json", "/greet", "/add", "/weather", "/mcp/sse"]
+    }
 
 # Add Greeting tool in the tools directory
 @mcp.tool(name="greet", description="Greet the user with a message")
